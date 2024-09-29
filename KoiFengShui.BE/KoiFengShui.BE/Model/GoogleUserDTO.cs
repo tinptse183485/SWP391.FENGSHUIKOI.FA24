21d9f9350d61911646c7076698d888241d380998
tinptse183485 <156059192+tinptse183485@users.noreply.github.com>
2024-09-29 23:10:33 +0700

    Revert "Hau"

--- KoiFengShui.BE/KoiFengShui.BE/Model/GoogleUserDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KoiFengShui.BE.Model
-{
-    public class GoogleUserDTO
-    {
-		public string GoogleId { get; set; }
-		public string Email { get; set; }
-		public string Name { get; set; }
-	}
-}
--- KoiFengShui.BE/KoiFengShui.BE/Model/LoginDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KoiFengShui.BE.Model
-{
-	public class LoginDTO
-	{
-		public string UserId { get; set; }
-		public string Password { get; set; }
-	}
-}
